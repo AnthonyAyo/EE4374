d99364a1459cc7c81bf538ef72e16e274769007f
Anthony Ayo <agayo@miners.utep.edu>
2019-02-11 00:39:19 -0700

    working on tokenizer

--- AAyo_prog1/aayo_argtok.c
+++ AAyo_prog1/aayo_argtok.c
@@ -9,3 +9,119 @@ int string_length(char *p){
   }
   return p-pp;
 }
+
+//checks to see if the character being looked at is valid
+char is_valid_character(char c){
+  if((c>=33||c<=126)){
+    return 1;
+  }else
+    return 0;
+}
+
+// find the start of the next word
+int find_word_start(char *p, int pos){
+  int start = 0;
+  int count = 0;
+  p +=pos;
+  char *str = p;
+
+  while(*str){
+    if(*str!=' '){
+      str++;
+    count++;
+    }else if ((*str ==' ')&&(*(str+1)==' ')){
+      str++;
+      count++;
+    }else if ((*str==' ')&&(*(str+1)!=' ')){
+      start = count+1;
+      return start;
+    }
+  }
+}
+
+//gets the starting position from find_word_start and continues on until a space is found
+
+int find_word_end(char *p, int pos){
+  int count = 0;
+  int end = 0;
+  char *str = &p[pos];
+
+  while(*str){
+    if(*str!=' '){
+      str++;
+      count++;
+    }else if((*str==' ')&&(*(str-1)!=' ')){
+      end = count-1;
+      return end;
+    }
+  }
+}
+
+// Count the ammount of words in the user input
+int count_words(char *p){
+  int wordCount = 0;
+  char *t=p;
+  int length = 0;
+  while(*t){
+    if(*t == ' '}{
+      length = 0;
+    }else if (++legth == 1){
+      wordCount++;
+      *t++;
+    }
+  }
+  return wordCount;
+}
+void print_tokens(char **p){
+  char **temp = p;
+  while(*temp !=null){
+    printf("The tokenized words are :%cn",*temp);
+  }
+}
+
+// free the space used up by malloc
+void free_tokens(char **p){
+  char **array = p;
+  while(*array){
+    free(*array);
+  }
+}
+
+char** argtok(char *p){
+  int numberOfWords = count_words(p);
+  int count = 0;
+  int start;
+  int end;
+  int wordLength;
+  int position = 0;
+  char ** tokens = (char **)malloc((numberOfWords+1)*sizeof(char*));
+  for(int i = 0; i < numberOfWords; i++){
+      start = find_word_start(p,position);
+      end = find_word_end(p,start);
+      wordLength = end - start;
+      char *word = (char *)malloc((wordLength+1)*sizeof(char*));
+      for (int j = start; j < end; j++){
+	*(word+count) = *(p+start);
+	count++;
+	start++;
+      }
+	*(tokens+i) = word;
+      position = end+1;
+  }
+  return tokens;
+}
+  
+
+
+
+
+
+
+       
+
+
+
+
+
+  
+
--- AAyo_prog1/aayo_argtok.h
+++ AAyo_prog1/aayo_argtok.h
@@ -1,1 +1,30 @@
 #ifndef _TOKENIZER_
+#define _TOKENIZER_
+
+// Count number of chars in the string
+int string_length(char*);
+
+//make sure the character c is a valid character to tokenize
+char is_valid_character(char c);
+
+//find the starting index for the next word
+int find_word_start(char*,int);
+
+//find the ending index for the next word
+int find_word_end(char*, int);
+
+//count number of strings in the string argument
+int count_words(char*);
+
+//print tokens
+void print_tokens(char**);
+
+// free all tokens and the array containing the tokens
+void free_tokens(char**);
+
+//tokenize the string into an array
+char** argtok(char*);
+
+#endif
+
+  
